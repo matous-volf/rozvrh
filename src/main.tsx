1079d0a02b819609d1f820afbca73d0426f24693
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-09-09 21:40:56 +0200

    Add the `CookiesProvider`

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
+import {CookiesProvider} from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App/>
+        <CookiesProvider>
+            <App/>
+        </CookiesProvider>
     </React.StrictMode>,
 )
