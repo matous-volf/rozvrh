e23f243bd31f1689fd5416c6d4884490d8a9c24b
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-09-09 21:42:37 +0200

    Create the group settings component

---
+++ src/components/GroupSettings.tsx
@@ -0,0 +1,67 @@
+import Timetable from "../models/Timetable.ts";
+import {ChangeEvent, useEffect, useState} from "react";
+import {nanoid} from "nanoid";
+
+interface Props {
+    timetable: Timetable;
+    selectedGroups: string[];
+    setSelectedGroupsCallback: (groups: string[]) => void;
+}
+
+function GroupSettings(props: Props) {
+    const [groups, setGroups] = useState<string[]>([]);
+    const [selectedGroups, setSelectedGroups] = useState<string[]>(props.selectedGroups);
+
+    useEffect(() => {
+        const groups: string[] = [];
+        for (const day of props.timetable.days) {
+            for (const hour of day.hours) {
+                for (const lesson of hour.lessons) {
+                    if (lesson.group === null || lesson.group.trim() === "" || groups.includes(lesson.group)) {
+                        continue;
+                    }
+
+                    groups.push(lesson.group);
+                }
+            }
+        }
+        setGroups(groups);
+    }, [props.timetable.days]);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const group = e.target.value;
+
+        setSelectedGroups((prevSelectedGroups) => {
+            const newSelectedGroups = [...prevSelectedGroups];
+
+            if (e.target.checked && !newSelectedGroups.includes(group)) {
+                newSelectedGroups.push(group);
+            }
+            if (!e.target.checked && newSelectedGroups.includes(group)) {
+                const index = newSelectedGroups.indexOf(group);
+                newSelectedGroups.splice(index, 1);
+            }
+
+            return newSelectedGroups;
+        });
+    };
+
+    const setSelectedGroupsCallback = props.setSelectedGroupsCallback;
+    useEffect(() => {
+        setSelectedGroupsCallback(selectedGroups);
+    }, [setSelectedGroupsCallback, selectedGroups]);
+
+    return (
+        <>
+            {groups.sort().map((group) => (
+                <div key={nanoid()}>
+                    <input type="checkbox" name="groups" value={group} id={"input_groups_" + group}
+                           onChange={handleChange} checked={selectedGroups.includes(group)}/>
+                    <label htmlFor={"input_groups_" + group}>{group}</label>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default GroupSettings
