55a84b02628103d30749ccfeb110feb533eb9473
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-09-17 20:53:22 +0200

    Enable Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import {CookiesProvider} from "react-cookie";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import ReactGA from "react-ga4";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import {Settings} from "luxon";
 
 Settings.defaultZone = "Europe/Prague";
-
 const queryClient = new QueryClient();
+ReactGA.initialize("G-60HTHGY4JH");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
