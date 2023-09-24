dffbe687b9d5096858fc5a18804b62cdb87543aa
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-09-24 14:19:37 +0200

    Remove a blank line

--- src/components/GroupSettings.tsx
+++ src/components/GroupSettings.tsx
@@ -49,7 +49,6 @@ function GroupSettings(props: Props) {
                            checked={selectedGroups.includes(group)} label={group} key={nanoid()}>
                 </FormCheck>
             ))}
-
         </>
     );
 }
