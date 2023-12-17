4ea412f941a37ffa6279146e6293a87f391f9a25
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-12-17 14:05:59 +0100

    make the schools URL a constant variable

--- src/api/schools.ts
+++ src/api/schools.ts
@@ -1,7 +1,9 @@
 import School from "../models/School.ts";
 
+const schoolsUrl = "https://vitskalicky.gitlab.io/bakalari-schools-list/schoolsList.json";
+
 export async function getSchools(): Promise<School[]> {
-    const response = await fetch("https://vitskalicky.gitlab.io/bakalari-schools-list/schoolsList.json");
+    const response = await fetch(schoolsUrl);
     if (!response.ok) {
         throw new Error();
     }
