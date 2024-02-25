675a255c4e5b13140523be211a4555b8ef69ba92
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-02-25 20:55:01 +0100

    create a teacher model

---
+++ src/models/Teacher.ts
@@ -0,0 +1,10 @@
+class Teacher {
+    id: string;
+    name: string;
+    constructor(id: string, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+export default Teacher;
