f6d8f21c835c45f7388774786a95f3347fbe2c91
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-03-10 13:34:42 +0100

    use a hook for setting the local storage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,53 +1,37 @@
-import {useCallback, useMemo, useState} from "react";
+import {useCallback, useMemo} from "react";
 import MainPage from "./MainPage.tsx";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import SettingsPage from "./SettingsPage.tsx";
 import {useQuery} from "@tanstack/react-query";
 import {getTimetable} from "../api/timetable.ts";
 import School from "../models/School.ts";
-
-function setLocalStorage<T extends T2, T2>(
-    key: string,
-    value: T,
-    setState: React.Dispatch<React.SetStateAction<T2>>
-) {
-    localStorage.setItem(key, JSON.stringify(value));
-    setState(value);
-}
+import {useLocalStorage} from "usehooks-ts";
 
 function App() {
-    const [selectedSchool, setSelectedSchool] = useState<School | null>(
-        JSON.parse(localStorage.getItem("selectedSchool")!)
-    );
-
-    const [selectedClassId, setSelectedClassId] = useState<string | null>(
-        JSON.parse(localStorage.getItem("selectedClassId")!)
-    );
-
-    const [selectedGroupIds, setSelectedGroupIds] = useState<string[]>(
-        JSON.parse(localStorage.getItem("selectedGroupIds")!) || []
-    );
+    const [selectedSchool, setSelectedSchool] = useLocalStorage<School | null>("selectedSchool", null);
+    const [selectedClassId, setSelectedClassId] = useLocalStorage<string | null>("selectedClassId", null);
+    const [selectedGroupIds, setSelectedGroupIds] = useLocalStorage<string[]>("selectedGroupIds", []);
 
     const handleSelectedSchoolChange = useCallback((school: School | null) => {
             if (school?.id !== selectedSchool?.id) {
-                setLocalStorage("selectedClassId", null, setSelectedClassId);
-                setLocalStorage("selectedGroupIds", [], setSelectedGroupIds);
-                setLocalStorage("selectedSchool", school, setSelectedSchool);
+                setSelectedClassId(null);
+                setSelectedGroupIds([]);
+                setSelectedSchool(school);
             }
-        }, [selectedSchool]
+        }, [selectedSchool?.id, setSelectedClassId, setSelectedGroupIds, setSelectedSchool]
     );
 
     const handleSelectedClassIdChange = useCallback((classId: string | null) => {
             if (classId !== selectedClassId) {
-                setLocalStorage("selectedGroupIds", [], setSelectedGroupIds);
-                setLocalStorage("selectedClassId", classId, setSelectedClassId);
+                setSelectedGroupIds([]);
+                setSelectedClassId(classId);
             }
-        }, [selectedClassId]
+        }, [selectedClassId, setSelectedClassId, setSelectedGroupIds]
     );
 
     const handleSelectedGroupIdsChange = useCallback((groupIds: string[]) => {
-            setLocalStorage("selectedGroupIds", groupIds, setSelectedGroupIds);
-        }, []
+            setSelectedGroupIds(groupIds);
+        }, [setSelectedGroupIds]
     );
 
     const timetableQuery = useQuery({
@@ -63,25 +47,25 @@ function App() {
     const timetable = timetableQuery.data === undefined ? null : timetableQuery.data;
 
     const router = useMemo(() => createBrowserRouter([
-                {
-                    path: "/",
-                    element: <MainPage timetable={timetable} isQueryLoading={timetableQuery.isLoading}
-                            isQueryError={timetableQuery.isError}/>,
-                },
-                {
-                    path: "/nastaveni",
-                    element: <SettingsPage isTimetableQueryLoading={timetableQuery.isLoading}
-                            isTimetableQueryError={timetableQuery.isError}
-                            timetable={timetable}
-                            selectedSchool={selectedSchool}
-                            setSelectedSchoolCallback={handleSelectedSchoolChange}
-                            selectedClassId={selectedClassId}
-                            selectedGroupIds={selectedGroupIds}
-                            setSelectedClassIdCallback={handleSelectedClassIdChange}
-                            setSelectedGroupIdsCallback={handleSelectedGroupIdsChange}/>,
-                },
-            ]), [timetable, timetableQuery, selectedSchool, handleSelectedSchoolChange, selectedClassId, selectedGroupIds,
-            handleSelectedClassIdChange, handleSelectedGroupIdsChange]);
+        {
+            path: "/",
+            element: <MainPage timetable={timetable} isQueryLoading={timetableQuery.isLoading}
+                               isQueryError={timetableQuery.isError}/>,
+        },
+        {
+            path: "/nastaveni",
+            element: <SettingsPage isTimetableQueryLoading={timetableQuery.isLoading}
+                                   isTimetableQueryError={timetableQuery.isError}
+                                   timetable={timetable}
+                                   selectedSchool={selectedSchool}
+                                   setSelectedSchoolCallback={handleSelectedSchoolChange}
+                                   selectedClassId={selectedClassId}
+                                   selectedGroupIds={selectedGroupIds}
+                                   setSelectedClassIdCallback={handleSelectedClassIdChange}
+                                   setSelectedGroupIdsCallback={handleSelectedGroupIdsChange}/>,
+        },
+    ]), [timetable, timetableQuery, selectedSchool, handleSelectedSchoolChange, selectedClassId, selectedGroupIds,
+        handleSelectedClassIdChange, handleSelectedGroupIdsChange]);
 
     return (
         <div className="h-100">
