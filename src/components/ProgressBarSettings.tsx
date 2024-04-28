19ff4b720bf6d71110d11606ad4792d362676bee
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-04-28 21:58:31 +0200

    create a progress bar settings component

---
+++ src/components/ProgressBarSettings.tsx
@@ -0,0 +1,20 @@
+import {FormCheck} from "react-bootstrap";
+import {useLocalStorage} from "usehooks-ts";
+
+function ProgressBarSettings() {
+    const [progressBarEnabled, setProgressBarEnabled] = useLocalStorage<boolean>("progressBarEnabled", true);
+
+    return (
+        <>
+            <h3>Ukazatel průběhu</h3>
+
+            <FormCheck label="zobrazit" type="radio" id="input-progress-bar-enabled"
+                       onChange={(e) => setProgressBarEnabled(e.target.checked)} checked={progressBarEnabled}/>
+
+            <FormCheck label="skrýt" type="radio" id="input-progress-bar-disabled"
+                       onChange={(e) => setProgressBarEnabled(!e.target.checked)} checked={!progressBarEnabled}/>
+        </>
+    );
+}
+
+export default ProgressBarSettings;
