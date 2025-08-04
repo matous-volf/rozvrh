6c80291bdad70476d0ed60a74a39d8d019e4abf5
Matouš Volf <git@matousvolf.cz>
2025-08-04 15:24:28 +0200

    feat: use Umami for analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./components/App.tsx"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import ReactGA from "react-ga4";
 import "./assets/css/style.css";
 
 import "./assets/css/bootstrap-custom.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import {Settings} from "luxon";
+import Helmet from "react-helmet";
 
 Settings.defaultZone = "Europe/Prague";
 const queryClient = new QueryClient({
@@ -18,12 +18,16 @@ const queryClient = new QueryClient({
         },
     },
 });
-ReactGA.initialize("G-60HTHGY4JH");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
+            <Helmet>
+                <script src="https://analytics.matousvolf.cz/script.js"
+                        data-website-id="a807ef9a-d4b3-4e0b-bee3-2cec07d6e39f" data-domains="rozvrh.matousvolf.cz"
+                        defer></script>
+            </Helmet>
             <App/>
         </QueryClientProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
 )
